b92700d824e0c1ffeb530110f43d5a3ff285b851
noahpessell <nwpessell@mail.com>
2024-10-05 00:06:08 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 6;
+        if(numToExamine % numToExamine == 0)
+        {
+            System.out.println("Your number is even.");
+        }
+        else
+        {
+            System.out.println("Your number is odd.");
+        }
+    }
+}
